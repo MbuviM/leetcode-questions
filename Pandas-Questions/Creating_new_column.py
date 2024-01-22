c3b2dc2aa48e3f9e116e57b840b7dd82195e5d50
MbuviM <mwendembuvi10@gmail.com>
2024-01-22 13:38:44 +0300

    Creating a new column

--- Pandas-Questions/Creating_new_column.py
+++ Pandas-Questions/Creating_new_column.py
@@ -10,3 +10,11 @@ A company plans to provide its employees with a bonus.
 
 Write a solution to create a new column name bonus that contains the doubled values of the salary column.
 """
+# N.B: insert() method is used to insert a new column at a specified place in the dataset.
+
+import pandas as pd
+
+def createBonusColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    
+    employees['bonus'] = employees ['salary'] * 2
+    return employees
