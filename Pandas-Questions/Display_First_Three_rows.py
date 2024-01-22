e8dc20381664e01c2167e616cb2dc5c7c02abfd7
MbuviM <mwendembuvi10@gmail.com>
2024-01-22 13:05:17 +0300

    Displaying the first three rows in a dataframe

---
+++ Pandas-Questions/Display_First_Three_rows.py
@@ -0,0 +1,18 @@
+"""
+DataFrame: employees
++-------------+--------+
+| Column Name | Type   |
++-------------+--------+
+| employee_id | int    |
+| name        | object |
+| department  | object |
+| salary      | int    |
++-------------+--------+
+Write a solution to display the first 3 rows of this DataFrame.
+"""
+import pandas as pd
+
+def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
+    
+    first_three = employees.head(3)
+    return first_three
