2ca97f92239255228c5f6c934a7ec70a74a81e40
MbuviM <mwendembuvi10@gmail.com>
2024-01-22 12:57:56 +0300

    Displaying the size of a dataframe as a list

---
+++ Pandas-Questions/Size_of_dataframe.py
@@ -0,0 +1,15 @@
+"""
+Write a solution to calculate and display the number of rows and columns of players.
+
+Return the result as an array:
+
+[number of rows, number of columns]
+"""
+
+import pandas as pd
+
+def getDataframeSize(players: pd.DataFrame) -> list[int]:
+
+    return ([len(players), len(players.columns)])
+
+    
