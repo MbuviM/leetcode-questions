1ccbd4f26edcc2f530485d4656f5b3d427b8b61e
MbuviM <mwendembuvi10@gmail.com>
2024-01-23 06:04:45 +0300

    Dropping missing data

---
+++ Pandas-Questions/Dropping_missing_data.py
@@ -0,0 +1,22 @@
+"""
+DataFrame students
++-------------+--------+
+| Column Name | Type   |
++-------------+--------+
+| student_id  | int    |
+| name        | object |
+| age         | int    |
++-------------+--------+
+There are some rows having missing values in the name column.
+
+Write a solution to remove the rows with missing values.
+
+"""
+
+import pandas as pd
+
+def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
+
+    students = students.dropna(subset="name", axis=0, how='any')
+
+    return students
