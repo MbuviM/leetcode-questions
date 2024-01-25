abcb25e87f42d681490839b2aa81a92ba404c307
MbuviM <mwendembuvi10@gmail.com>
2024-01-25 23:04:07 +0300

    Sorting values in a column

---
+++ Pandas-Questions/Method_Chaining.py
@@ -0,0 +1,23 @@
+"""
+DataFrame animals
++-------------+--------+
+| Column Name | Type   |
++-------------+--------+
+| name        | object |
+| species     | object |
+| age         | int    |
+| weight      | int    |
++-------------+--------+
+Write a solution to list the names of animals that weigh strictly more than 100 kilograms.
+
+Return the animals sorted by weight in descending order.
+"""
+
+import pandas as pd
+
+def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
+    
+    filtered_animals = animals[animals['weight']> 100]
+    sorted_animals = filtered_animals.sort_values(by='weight', ascending=False)
+
+    return sorted_animals[['name']]
